ab27b05fd042db1baf3f62ef1114e8cf99987a46
Camilo Pimiento <camilopimientogarcia09@gmail.com>
2025-09-20 02:05:35 -0500

    Cambios Diseño Minimos

--- src/layouts/layoutsinfooter.vue
+++ src/layouts/layoutsinfooter.vue
@@ -1,3 +1,33 @@
 <template>
-    
+  <div class="layout">
+    <header>
+      <Navbar />
+    </header>
+
+    <!-- Contenido dinámico -->
+    <main class="contenido">
+      <router-view />
+    </main>
+  </div>
 </template>
+
+<script setup>
+import Navbar from "../components/navbar.vue";
+</script>
+
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; 
+}
+
+header {
+  width: 100%; 
+}
+
+.contenido {
+  flex: 1; 
+  padding: 1rem;
+}
+</style>
